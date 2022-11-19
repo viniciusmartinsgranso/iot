a5c42cb9d489073526b29bcaaee16b245a8c2f99
Vinícius Martins <viniciusmartinsg@hotmail.com>
2022-11-19 14:24:37 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'iot',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
